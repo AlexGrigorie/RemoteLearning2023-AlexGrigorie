fdfdfafb8e0af051752f2337dd38837272a619a3
AlexGrigorie <alexgabriel.1998@yahoo.com>
2023-12-16 09:42:00 +0200

    Open-close principle

---
+++ OpenClosePrinciple/Geometrix/sources/Geometrix.NoOcp/Geometrix.NoOcp/ShapeModel/Triangle.cs
@@ -0,0 +1,9 @@
+﻿namespace iQuest.Geometrix.NoOcp.ShapeModel
+{
+    internal class Triangle
+    {
+        public int LengthA { get; set; }
+        public int LengthB { get; set; }
+        public int LengthC { get; set; }
+    }
+}
---
+++ OpenClosePrinciple/Geometrix/sources/Geometrix.WithOcp/Geometrix.WithOcp/ShapeModel/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace iQuest.Geometrix.WithOcp.ShapeModel
+{
+    internal class Triangle : IShape
+    {
+        public int LengthA { get; set; }
+        public int LengthB { get; set; }
+        public int LengthC { get; set; }
+
+        public double CalculateArea()
+        {
+            return 0.25 * Math.Sqrt((LengthA + LengthB + LengthC) 
+                * (-LengthA + LengthB + LengthC) 
+                * (LengthA - LengthB + LengthC)
+                * (LengthA + LengthB - LengthC));
+        }
+    }
+}
