7d239f1ae39b14d57531b7e524b9fb0827b42c81
AlexGrigorie <alexgabriel.1998@yahoo.com>
2024-02-24 21:26:53 +0200

    Equality and CompareTo

---
+++ Terra - Object Equality/Terra/Country.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace iQuest.Terra
+{
+    public class Country : IComparable
+    {
+        public string Name { get; }
+
+        public string Capital { get; }
+
+        public Country(string name, string capital)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Capital = capital ?? throw new ArgumentNullException(nameof(capital));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Country country = obj as Country;
+            if (country == null)
+                return false;
+
+            return Name == country.Name
+                && Capital == country.Capital;
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            if(!(obj is Country)) throw new ArgumentException("The object must be Country type");
+
+            return Name.CompareTo((obj as Country).Name);
+        }
+
+    }
+}
