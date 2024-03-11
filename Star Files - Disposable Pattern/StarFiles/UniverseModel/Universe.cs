b0c9ffd0f3679271b2fbdefd236a3c242de9b62c
AlexGrigorie <alexgabriel.1998@yahoo.com>
2024-03-11 21:16:27 +0200

    Implemented Disposable Pattern

---
+++ Star Files - Disposable Pattern/StarFiles/UniverseModel/Universe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iQuest.StarFiles.UniverseModel
+{
+    internal sealed class Universe
+    {
+        private readonly List<SimpleStar> stars = new List<SimpleStar>();
+
+        public string CreateStarFromTemplate(string name)
+        {
+            using (SimpleStar star = new SimpleStar(name))
+            {
+                stars.Add(star);
+                return star.FileName;
+            }
+        }
+
+        public Tuple<string, string> CreateBinaryStar(string name)
+        {
+            using (BinaryStar star = new BinaryStar(name))
+            {
+                stars.Add(star);
+                return new Tuple<string, string>(star.FileName, star.AdditionalFilename);
+            }
+        }
+    }
+}
