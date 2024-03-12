2a7f157f6305abe871457204f306d778feec3aca
AlexGrigorie <alexgabriel.1998@yahoo.com>
2024-03-12 11:27:19 +0200

    Refactoring

--- Eyeglass - Reflection/Reflection/Program.cs
+++ Eyeglass - Reflection/Reflection/Program.cs
@@ -16,65 +16,8 @@ namespace Reflection
                 foreach (Type type in types)
                 {
                     Console.WriteLine($"Type: {type.FullName}");
-                    if (type.IsClass)
-                    {
-                        Console.WriteLine($"Class Name: {type.Name}");
-                        ConstructorInfo[] constructors = type.GetConstructors();
-                        foreach (ConstructorInfo constructor in constructors)
-                        {
-                            Console.WriteLine($"Constructor: {constructor.Name}");
-                            ParameterInfo[] parameters = constructor.GetParameters();
-                            foreach (ParameterInfo parameter in parameters)
-                            {
-                                Console.WriteLine($"Param: {parameter.ParameterType} {parameter.Name} {parameter}");
-                            }
-                        }
-                    }
-                    else if (type.IsInterface)
-                    {
-                        Console.WriteLine($"Interface Name: {type.Name}");
-                        MethodInfo[] methods = type.GetMethods();
-                        foreach (MethodInfo method in methods)
-                        {
-                            Console.WriteLine($"Method: {method.ReturnType} {method.Name}");
-                            ParameterInfo[] parameters = method.GetParameters();
-                            foreach (ParameterInfo parameter in parameters)
-                            {
-                                Console.WriteLine($"Param: {parameter.ParameterType} {parameter.Name}");
-                            }
-                        }
-                    }
-                    else if (type.IsEnum)
-                    {
-                        Console.WriteLine($"Enum Name: {type.Name}");
-                        var enumValues = Enum.GetValues(type);
-                        foreach (var value in enumValues)
-                        {
-                            Console.WriteLine($"{value}: {(int)value}");
-                        }
-                    }
-
-                    MemberInfo[] members = type.GetMembers();
-                    foreach (MemberInfo member in members)
-                    {
-                        switch (member.MemberType)
-                        {
-                            case MemberTypes.Property:
-                                Console.WriteLine($"Property: {member.Name} {member.MemberType}");
-                                break;
-                            case MemberTypes.Method:
-                                Console.WriteLine($"Method: {member.Name} {member.MemberType} {member.DeclaringType}");
-                                break;
-                            case MemberTypes.Event:
-                                Console.WriteLine($"Event: {member.Name} {member.MemberType} {member.DeclaringType}");
-                                break;
-                            case MemberTypes.Field:
-                                Console.WriteLine($"Field: {member.Name} {member.MemberType} {member.DeclaringType}");
-                                break;
-                            default:
-                                break;
-                        }
-                    }
+                    ShowTypeInformation(type);
+                    MembersInfomation(type);
                 }
             }
             catch (Exception ex)
@@ -82,5 +25,86 @@ namespace Reflection
                 Console.WriteLine($"Error: {ex.Message}");
             }
         }
+
+        private static void MembersInfomation(Type type)
+        {
+            MemberInfo[] members = type.GetMembers();
+            foreach (MemberInfo member in members)
+            {
+                switch (member.MemberType)
+                {
+                    case MemberTypes.Property:
+                        Console.WriteLine($"Property: {member.Name} {member.MemberType}");
+                        break;
+                    case MemberTypes.Method:
+                        Console.WriteLine($"Method: {member.Name} {member.MemberType} {member.DeclaringType}");
+                        break;
+                    case MemberTypes.Event:
+                        Console.WriteLine($"Event: {member.Name} {member.MemberType} {member.DeclaringType}");
+                        break;
+                    case MemberTypes.Field:
+                        Console.WriteLine($"Field: {member.Name} {member.MemberType} {member.DeclaringType}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void ShowTypeInformation(Type type)
+        {
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Object when type.IsClass:
+                    ShowClassInformation(type);
+                    break;
+                case TypeCode.Object when type.IsInterface:
+                    ShowInterfaceInformation(type);
+                    break;
+                case TypeCode.Object when type.IsEnum:
+                    ShowEnumInformation(type);
+                    break;
+            }
+        }
+        private static void ShowClassInformation(Type type)
+        {
+            Console.WriteLine($"Class Name: {type.Name}");
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine($"Constructor: {constructor.Name}");
+                ShowParameters(constructor.GetParameters());
+            }
+        }
+
+        private static void ShowInterfaceInformation(Type type)
+        {
+            Console.WriteLine($"Interface Name: {type.Name}");
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine($"Method: {method.ReturnType} {method.Name}");
+                ShowParameters(method.GetParameters());
+            }
+        }
+
+        private static void ShowEnumInformation(Type type)
+        {
+            Console.WriteLine($"Enum Name: {type.Name}");
+            var enumValues = Enum.GetValues(type);
+            foreach (var value in enumValues)
+            {
+                Console.WriteLine($"{value}: {(int)value}");
+            }
+        }
+
+        private static void ShowParameters(ParameterInfo[] parameters)
+        {
+            foreach (ParameterInfo parameter in parameters)
+            {
+                Console.WriteLine($"Param: {parameter.ParameterType} {parameter.Name}");
+            }
+        }
+
     }
 }
